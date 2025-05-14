f0e4603c24e908e9477064de628f8f01d0e4a06c
devorack <jhoanmarquina@gmail.com>
2025-05-14 17:11:31 -0400

    eliminando archivos

--- Practica 2/old/ec_diff.m
+++
@@ -1,4 +0,0 @@
-function dVc_dt = ec_differential(t, Vc, R2, Req,Rm, E, C,u)
-    % Ecuación diferencial para el circuito
-    dVc_dt = ( (R2 * E * u) - ((R2 +Req) * (Vc) )) / (C * ((Rm * Req) + (R2* Req)+ (Rm*R2))); % dVc/dt
-end
